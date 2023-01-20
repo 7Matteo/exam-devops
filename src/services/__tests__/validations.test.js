d410466c23cc39ecaa9c519f0756f9e3ec99146e
7Matteo <matteo.fiore@student.vinci.be>
2023-01-20 10:46:58 +0100

    test : label contains at least 1 special char

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -20,11 +20,19 @@ describe("validations tests suites - isEmpty", () => {
 
 describe("validations tests suites - isValid", () => {
     test("should return true as the label is 9 chars lenght", () => {
-        const result = isValid("aaaaaaaaa");
+        const result = isValid("aaaaaaaaa&");
         expect(result).toBe(true);
     });
     test("should return false as the label is 6 chars lenght", () => {
         const result = isValid("aaaaaa");
+        expect(result).toBe(false);
+    });
+    test("should return true as the label contains at least 1 special char", () => {
+        const result = isValid("aaaaaa&&&&&&&");
+        expect(result).toBe(true);
+    });
+    test("should return false as the label contains 0 special char", () => {
+        const result = isValid("aaaaaaaaaaa");
         expect(result).toBe(false);
     });
 });
